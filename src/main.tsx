d2fdd632fb019d72b8183e7bbf4e7a534ff27242
VMang0 <valeriakorolkova2703@gmail.com>
2024-05-02 00:52:04 +0300

    feat: add settings for linter and prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx'
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </StrictMode>,
+);
